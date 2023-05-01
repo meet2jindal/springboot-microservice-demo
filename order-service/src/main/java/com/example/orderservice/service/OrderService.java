9eeaf4f4f0a21eca83a9c8be2793e9aece0000ff
Naresh_Jindal <naresh_jindal@epam.com>
2023-05-01 18:30:49 +0530

    Added Hystrix

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -7,7 +7,6 @@ import com.example.orderservice.entity.Order;
 import com.example.orderservice.repository.OrderRepository;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,7 +20,7 @@ public class OrderService {
     private RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "paymentServiceDown")
-    public TransactionResponse saveOrder(TransactionRequest request){
+    public TransactionResponse saveOrder(TransactionRequest request) {
         String orderReponse;
         Order order = request.getOrder();
         Payment payment = request.getPayment() != null ? request.getPayment() : new Payment();
@@ -29,17 +28,17 @@ public class OrderService {
         payment.setAmount(order.getPrice());
 
         Payment paymentReponse = restTemplate.postForObject("http://PAYMENT-SERVICE/payment/doPayment", payment, Payment.class);
-        orderReponse = paymentReponse.getPaymentStatus().equals("success")? "Order success" : "order fail";
+        orderReponse = paymentReponse != null && paymentReponse.getPaymentStatus().equals("success") ? "Order success" : "order fail";
         orderRepository.save(order);
         TransactionResponse transactionResponse = new TransactionResponse();
         transactionResponse.setOrder(order);
         transactionResponse.setAmount(order.getPrice());
-        transactionResponse.setTransactionId(paymentReponse.getTransactionId());
+        transactionResponse.setTransactionId(paymentReponse != null ? paymentReponse.getTransactionId() : null);
         transactionResponse.setOrderStatus(orderReponse);
         return transactionResponse;
     }
 
-    public TransactionResponse paymentServiceDown(TransactionRequest request){
+    public TransactionResponse paymentServiceDown(TransactionRequest request) {
 
         TransactionResponse transactionResponse = new TransactionResponse();
         transactionResponse.setOrderStatus("Failed- Payment service unavailable");
