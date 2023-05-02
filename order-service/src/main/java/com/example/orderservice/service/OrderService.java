5efb953dc47baa3267bacbd5d38c327fd5203bd2
Naresh_Jindal <naresh_jindal@epam.com>
2023-05-02 19:10:07 +0530

    Upgraded to java 17 and Added ELK stack

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -5,7 +5,11 @@ import com.example.orderservice.common.TransactionRequest;
 import com.example.orderservice.common.TransactionResponse;
 import com.example.orderservice.entity.Order;
 import com.example.orderservice.repository.OrderRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -18,6 +22,8 @@ import org.springframework.web.client.RestTemplate;
 @RefreshScope
 public class OrderService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderService.class);
+
     @Autowired
     private OrderRepository orderRepository;
 
@@ -27,21 +33,26 @@ public class OrderService {
 
     @Value("${microservice.payment-service.endpoints.endpoint.uri}")
     private String PAYMENT_SERVICE_ENDPOINT;
+
     @HystrixCommand(fallbackMethod = "paymentServiceDown")
     public TransactionResponse saveOrder(TransactionRequest request) {
         String orderReponse;
         Order order = request.getOrder();
+
+        LOGGER.info("Order Request " + order);
         Payment payment = request.getPayment() != null ? request.getPayment() : new Payment();
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
 
-        Payment paymentReponse = restTemplate.postForObject(PAYMENT_SERVICE_ENDPOINT, payment, Payment.class);
-        orderReponse = paymentReponse != null && paymentReponse.getPaymentStatus().equals("success") ? "Order success" : "order fail";
+        Payment paymentResponse = restTemplate.postForObject(PAYMENT_SERVICE_ENDPOINT, payment, Payment.class);
+        LOGGER.info("Paymnet Response for rest call from order service " + paymentResponse);
+        orderReponse = paymentResponse != null && paymentResponse.getPaymentStatus().equals("success") ? "Order success" : "order fail";
+
         orderRepository.save(order);
         TransactionResponse transactionResponse = new TransactionResponse();
         transactionResponse.setOrder(order);
         transactionResponse.setAmount(order.getPrice());
-        transactionResponse.setTransactionId(paymentReponse != null ? paymentReponse.getTransactionId() : null);
+        transactionResponse.setTransactionId(paymentResponse != null ? paymentResponse.getTransactionId() : null);
         transactionResponse.setOrderStatus(orderReponse);
         return transactionResponse;
     }
